9a1d34da72a2694bad3d1d56c8eb3df0805fb723
Jasmine Hendricks <jhendricks@mparticle.com>
2024-06-26 22:42:57 -0600

    Template is now plug and play, no components

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App: React.FC = () => {
+  return <div>Hello World, I'm going to be an awesome app someday 💪🏾</div>;
+};
+ReactDOM.render(<App />, document.getElementById("root"));
